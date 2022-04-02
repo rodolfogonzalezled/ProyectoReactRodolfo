cc41c27e49d234b75273e059ac9bd6fa86d5c725
Rodolfo Gonzalez <rgonzalezled@gmail.com>
2022-04-01 23:44:07 -0300

    Correccion de estilos y cambio de iconos en botones

--- src/Componets/ItemCount/ItemCount.js
+++ src/Componets/ItemCount/ItemCount.js
@@ -1,16 +1,17 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import './ItemCount.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './ItemCount.css'
 
-const ItemCount = ({stock, initialCount = 1, onAdd}) => {
+
+const ItemCount = ({ stock, initialCount = 1, onAdd }) => {
     const [count, setCount] = useState(initialCount);
-    
+
     const increment = () => {
         if (count < stock) {
-            setCount(count + 1);  
-        }else{
+            setCount(count + 1);
+        } else {
             toast.warn(`Solo hay disponible ${stock} unidades de éste producto`, {
                 position: "bottom-center",
                 autoClose: 2000,
@@ -18,27 +19,27 @@ const ItemCount = ({stock, initialCount = 1, onAdd}) => {
                 closeOnClick: true,
                 pauseOnHover: false,
                 draggable: true,
-                progress: undefined, 
-                theme: "dark",
-                });
+                progress: undefined,
+                theme: "light",
+            });
         }
     };
 
-    const decrement = () => { 
-        if(count > 0){
-            setCount(count - 1);  
+    const decrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
         }
     };
 
     return (
-        <div>
-            <div className='itemCount'>
-                <Button onClick={decrement} variant="outline-secondary" size="lg"> - </Button>
-                <h2>{count}</h2>
-                <Button onClick={increment} variant="outline-secondary" size="lg"> + </Button>
+        <div className='ContainerCount'>
+            <div className='ItemCount'>
+                <Button onClick={decrement} variant="outline-secondary" size="lg"> ➖ </Button>
+                <h2 className='Count' >{count}</h2>
+                <Button onClick={increment} variant="outline-secondary" size="lg"> ➕ </Button>
                 <ToastContainer />
             </div>
-            <Button onClick={() => onAdd(count)} className='addCart' variant="success">Agregar al carrito</Button>
+            <Button onClick={() => onAdd(count)} className='AddCart' variant="outline-success">Agregar al carrito</Button>
         </div>
     );
 };
